8f5bb634d82a6731e0e9450666d45a13e7387edc
Ger <togean@yandex.ru>
2024-09-16 18:32:56 +0300

    ДЗ номер 11 полный RestAPI, обработка ошибок, тесты все отрабатывают. Подправлен стиль

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface FilmService {
